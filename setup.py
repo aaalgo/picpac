53241ed9ac493deff329741b834ee8d1cc870aaf
Wei Dong <wdong@wdong.org>
2016-12-08 09:49:43 -0500

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ picpac = Extension('_picpac',
         libraries = ['opencv_highgui', 'opencv_core', 'boost_filesystem', 'boost_system', 'boost_python', 'glog'],
         library_dirs = ['/usr/local/lib'],
         sources = ['python-api.cpp', 'picpac.cpp', 'picpac-cv.cpp', 'json11/json11.cpp'],
-        depends = ['picpac.h', 'picpac-cv.h'])
+        depends = ['picpac.h', 'picpac-cv.h', 'json11/json11.hpp'])
 
 setup (name = 'picpac',
        version = '0.2',
